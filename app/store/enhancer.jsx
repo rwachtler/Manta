0692ba47d666c5451573b156853336b01ff85c4d
Ramis <ramis.wachtler@outlook.com>
2018-02-06 19:27:08 +0100

    Added enhancer for services

--- app/store/enhancer.jsx
+++ app/store/enhancer.jsx
@@ -2,10 +2,11 @@ import { getAllDocs } from '../helpers/pouchDB';
 
 // Retrieve Initial state from IndexDB
 const getInitialState = () =>
-  Promise.all([getAllDocs('contacts'), getAllDocs('invoices')])
+  Promise.all([getAllDocs('contacts'), getAllDocs('invoices'), getAllDocs('services')])
     .then(values => ({
       contacts: values[0],
       invoices: values[1],
+      services: values[2]
     }))
     .catch(err => console.log(err));
 
